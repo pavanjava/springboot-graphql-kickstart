95dde291d0ee9abe95a63c06df8029015f678b81
Pavan Mantha <manthapavankumar11@gmail.com>
2023-03-21 11:22:37 +0530

    initial version of working code of graphql

---
+++ graphql-app/src/test/java/com/sparkdev/graphqlapp/GraphqlAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparkdev.graphqlapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
